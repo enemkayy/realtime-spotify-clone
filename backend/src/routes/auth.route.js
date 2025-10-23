2066094b1b09ac50ca6df476481e6e40193310c6
enemkayy <nmkhoi.gdsciu@gmail.com>
2025-10-24 00:29:32 +0700

    add protectRoute middleware import to auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authCallback } from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
